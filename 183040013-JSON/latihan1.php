315a530e2da28eee790429c593a2e76be3929932
irit24 <tadzaka.10@gmail.com>
2020-11-16 20:42:45 +0700

    JSON
    Latihan Praktikum Rekayasa Web Petemuan 1

---
+++ 183040013-JSON/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama" => "ilyasa Ridho",
+        "nrp" => "183040013",
+        "email" => "tadzaka.10@gmail.com"
+    ],
+    [
+        "nama" => "ilyasa Ridho",
+        "nrp" => "183040013",
+        "email" => "tadzaka.10@gmail.com"
+    ]
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
---
+++ 183040013-JSON/script.js
@@ -0,0 +1,6 @@
+let mahasiswa ={
+    nama : "ilyasa Ridho",
+    nrp : "183040013",
+    email : "tadzaka.10@gmail.com"
+}
+console.log(JSON.stringify(mahasiswa));
